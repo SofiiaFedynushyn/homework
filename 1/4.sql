59e5e2e1ef585ee956cd9753112cb93ae24639fe
SofiiaFedynushyn <49729614+SofiiaFedynushyn@users.noreply.github.com>
2019-07-31 00:46:35 +0300

    Update 4.sql

--- 1/4.sql
+++ 1/4.sql
@@ -1,8 +1,15 @@
 USE [education]
 
-SELECT DISTINCT s1.[supplierid] AS First_Suplier, s2.[supplierid] AS Second_Suplier, s1.[detailid] AS First_detail, s2.[detailid] AS Second_detail 
-FROM [dbo].[supplies] AS s1
-INNER JOIN [dbo].[supplies] AS s2
-ON s1.[supplierid] = s2.[supplierid] 
-WHERE s1.[detailid] < s2.[detailid]	
-ORDER BY (First_Suplier), (Second_Suplier), (First_Detail), (Second_Detail)
+;WITH CTE (supplierid, detailid1, detailid2)
+AS
+(SELECT DISTINCT s1.supplierid, s1.detailid, s2.detailid FROM supplies AS s1
+INNER JOIN supplies AS s2
+ON s1.supplierid = s2.supplierid AND s2.detailid > s1.detailid)
+SELECT CTE1.supplierid AS First_Supplier
+      ,CTE2.supplierid AS Second_Supplier
+	  ,CTE1.detailid1 AS First_Detail
+	  ,CTE1.detailid2 AS Second_Detail
+FROM CTE AS CTE1
+INNER JOIN CTE AS CTE2
+ON CTE1.detailid1 = CTE2.detailid1 AND CTE1.detailid2 =CTE2.detailid2 AND CTE1.supplierid < CTE2.supplierid
+ORDER BY First_Supplier, Second_Supplier, First_Detail, Second_Detail
